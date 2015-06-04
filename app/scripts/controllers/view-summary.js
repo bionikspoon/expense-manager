c0d41cb0c99e434407160f7828f22fc429372c0f
Manu <bionikspoon@gmail.com>
2015-06-04 14:34:02 -0500

    add summary of categories to view

--- app/scripts/controllers/view-summary.js
+++ app/scripts/controllers/view-summary.js
@@ -9,6 +9,16 @@
  */
 angular.module('ExpenseManagerApp')
 
-  .controller('ViewSummaryCtrl', function ($scope, ExpenseService) {
+  .controller('ViewSummaryCtrl',
+  function ($scope, ExpenseService, CategoryList) {
     $scope.expenses = ExpenseService.getExpense();
+
+    $scope.summaryData = [];
+    CategoryList.forEach(function (category) {
+      var categoryTotal = ExpenseService.getCategoryTotal(category);
+      $scope.summaryData.push({
+        category: category,
+        amount: categoryTotal
+      });
+    });
   });
