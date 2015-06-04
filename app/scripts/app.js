0e93c5c5b26915c89b602e67fb12c3b22fd3c5bd
Manu <bionikspoon@gmail.com>
2015-06-04 01:23:13 -0500

    setup routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,7 @@
  * Main module of the application.
  */
 angular
+
   .module('ExpenseManagerApp', [
     'ngAnimate',
     'ngAria',
@@ -19,28 +20,25 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
+
   .config(function ($routeProvider) {
     $routeProvider
+
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/home', {
         templateUrl: 'views/home.html',
         controller: 'HomeCtrl'
       })
+
       .when('/add-expense', {
         templateUrl: 'views/add-expense.html',
         controller: 'AddExpenseCtrl'
       })
+
       .when('/view-summary', {
         templateUrl: 'views/view-summary.html',
         controller: 'ViewSummaryCtrl'
       })
+
       .otherwise({
         redirectTo: '/'
       });
